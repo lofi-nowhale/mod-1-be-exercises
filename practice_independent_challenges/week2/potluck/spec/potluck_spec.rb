e512f4af13fc767de9ef70cf28719dc8c2d039a8
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-07-12 21:58:20 -0600

    wip: setting up TDD for the add_dish method

--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -11,6 +11,17 @@ RSpec.describe Potluck do
   it 'has readable attributes' do 
     potluck = Potluck.new("7-13-18")
     expect(potluck.date).to eq("7-13-18")
+    expect(potluck.dishes).to eq([])
   end
-  
+
+  describe '#add_dish' do
+    it 'can add a new instance of the dish class' do
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+    expect(potluck.dishes).to eq([couscous_salad, cocktail_meatballs])
+    end
+  end
+
 end
