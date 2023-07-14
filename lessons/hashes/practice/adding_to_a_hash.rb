2ce0cd6b6425ce1485116ae99f3fba7ada0355db
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-07-14 10:18:13 -0600

    feat: Add lines to add to the suitcase hash

--- lessons/hashes/practice/adding_to_a_hash.rb
+++ lessons/hashes/practice/adding_to_a_hash.rb
@@ -5,8 +5,13 @@ suitcase = {
 
 # How can we add the following key/value pairs:
 # 1. jackets/2
+suitcase['jackets'] = 2
 # 2. shirts/4
+suitcase['shirts'] = 4
 # 3. shoes/2
+suitcase['shoes'] = 2
 
 # How can we increase the existing value:
 # 1. Change pants to have a value of 2
+suitcase['pants'] = 2
+require 'pry';binding.pry
