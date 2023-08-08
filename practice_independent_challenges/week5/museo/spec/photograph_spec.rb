5b908829e8364bba5c706dd0b43a70c574c62ba8
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-08-07 19:57:11 -0600

    fix: tests for the photograph method

--- practice_independent_challenges/week5/museo/spec/photograph_spec.rb
+++ practice_independent_challenges/week5/museo/spec/photograph_spec.rb
@@ -4,21 +4,20 @@ require './lib/artist'
 
 RSpec.describe Photograph do 
   before (:each) do 
-    @photograph = Photograph.new(attributes)
-
     attributes = {
       id: "1",
       name: "Rue Mouffetard, Paris (Boy with Bottles)",
       artist_id: "4",
       year: "1954"
     }
+
+    @photograph = Photograph.new(attributes)
   end
   it "exists and has readable attributes" do 
-    expect(@photograph).to be_a Photograph
-    expect(@photograph.attributes).to be_a Hash 
+    expect(@photograph).to be_a Photograph 
     expect(@photograph.id).to eq("1")
     expect(@photograph.name).to eq("Rue Mouffetard, Paris (Boy with Bottles)")
-    expect(photograph.artist_id).to eq("4")
+    expect(@photograph.artist_id).to eq("4")
     expect(@photograph.year).to eq("1954")
   end
 end
