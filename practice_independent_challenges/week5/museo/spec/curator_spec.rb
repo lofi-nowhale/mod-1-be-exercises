28608b0ec2d9922dede93825ce387d651d7bb9d3
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-08-07 20:28:28 -0600

    test: Add a test for the #add_photograph method

--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -2,25 +2,25 @@ require './lib/photograph'
 require './lib/artist'
 require './lib/curator'
 
-Rspec.describe Curator do 
+RSpec.describe Curator do 
   before (:each) do 
     @curator = Curator.new 
 
-    photo_1 = Photograph.new({
+    @photo_1 = Photograph.new({
       id: "1",      
       name: "Rue Mouffetard, Paris (Boy with Bottles)",      
       artist_id: "1",      
       year: "1954"      
     })   
 
-    photo_2 = Photograph.new({
+    @photo_2 = Photograph.new({
       id: "2",      
       name: "Moonrise, Hernandez",      
       artist_id: "2",      
       year: "1941"      
     })     
 
-    artist_1 = Artist.new({
+    @artist_1 = Artist.new({
       id: "1",      
       name: "Henri Cartier-Bresson",      
       born: "1908",      
@@ -28,7 +28,7 @@ Rspec.describe Curator do
       country: "France"
     }) 
 
-    artist_2 = Artist.new({
+    @artist_2 = Artist.new({
       id: "2",      
       name: "Ansel Adams",      
       born: "1902",      
@@ -41,5 +41,15 @@ Rspec.describe Curator do
   it "exists and has readable attributes" do 
     expect(@curator.photographs).to eq([])
     expect(@curator.artists).to eq([])
+  end
+
+  describe "#add_photograph" do 
+    it "can add a photo object to the photographs array for the curator" do
+      @curator.add_photograph(@photo_1)
+
+      @curator.add_photograph(@photo_2)
+      
+      expect(@curator.photographs).to eq([photo_1, photo_2])      
+    end
   end
 end
