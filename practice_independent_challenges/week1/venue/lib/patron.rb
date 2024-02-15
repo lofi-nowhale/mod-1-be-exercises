8869a65c89a86e3a38d4e7f8c747e077a40e8b3e
Noelle Hemphill <noelle.hemphill@gmail.com>
2024-02-15 13:53:27 -0700

    feat: finish venue practice

---
+++ practice_independent_challenges/week1/venue/lib/patron.rb
@@ -0,0 +1,7 @@
+# class Patron
+#   attr_reader :patron_name
+#   attr_accessor :patron_name
+#   def initialize(patron_name)
+#     @patron_name = patron_name
+#   end
+# end
---
+++ practice_independent_challenges/week1/venue/spec/patron_spec.rb
@@ -0,0 +1,14 @@
+# require 'rspec'
+# require './lib/patron.rb'
+
+# RSpec.describe Patron do
+#   it "exists" do
+#     patron = Patron.new("Mike")
+#     expect(patron).to be_a(Patron)
+#   end
+
+#   it "has readable attributes" do
+#     patron = Patron.new("Mike")
+#     expect(patron.patron_name).to eq("Mike")
+#   end
+# end
